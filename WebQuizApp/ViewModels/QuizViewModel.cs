35216792c3e985b974a93a27971fcfe5aea2bde1
iamkramzTech <bernaldezm88@yahoo.com>
2024-06-23 13:19:50 +0800

    Added ViewModels Folder for ViewModel class

---
+++ WebQuizApp/ViewModels/QuizViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WebQuizApp.Models;
+namespace WebQuizApp.ViewModels
+{
+    public class QuizViewModel
+    {
+        public List<TriviaQuestionModel>? TriviaQuestions { get; set; }
+        public List<string> UserAnswers { get; set; } = new List<string>();
+        public int Score { get; set; }
+    }
+}
